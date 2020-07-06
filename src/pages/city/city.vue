0d348b2024981f419d18467d4b9b50be7fe64b19
wsywsy <2238574812@qq.com>
2020-07-06 23:47:36 +0800

    导航列表 和bscroll和字母侧边栏完成但缺少页面逻辑

--- src/pages/city/city.vue
+++ src/pages/city/city.vue
@@ -4,17 +4,25 @@
 </cityheader>
 <citysearch>
 </citysearch>
+<citylist>
+</citylist>
+<cityzimu>
+</cityzimu>
 </div>
 </template>
 
 <script>
 import cityheader from './components/header'
 import citysearch from './components/search.vue'
+import citylist from './components/list.vue'
+import cityzimu from './components/zimubiao.vue'
 export default {
   name: 'city',
   components: {
     cityheader,
-    citysearch
+    citysearch,
+    citylist,
+    cityzimu
   }
 }
 </script>
---
+++ src/pages/city/components/zimubiao.vue
@@ -0,0 +1,51 @@
+<template>
+<div class="header">
+<ul>
+<li>A</li>
+<li>B</li>
+<li>C</li>
+<li>D</li>
+<li>E</li>
+<li>F</li>
+<li>G</li>
+<li>H</li>
+<li>I</li>
+<li>G</li>
+<li>K</li>
+<li>L</li>
+<li>M</li>
+<li>N</li>
+<li>O</li>
+<li>P</li>
+<li>Q</li>
+<li>R</li>
+<li>A</li>
+<li>A</li>
+<li>A</li>
+<li>A</li>
+<li>A</li>
+<li>A</li>
+</ul>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'cityzimu'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/variables.styl'
+  @import '~styles/mixins.styl'
+  .header
+    position: absolute
+    top: 1.73rem
+    bottom: 0
+    right: 0
+    display: flex
+    ul
+      align-self:center
+      li
+        line-height: .4rem
+</style>
