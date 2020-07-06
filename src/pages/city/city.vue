a508c7ccfd1361b423a4b2fb9b321d100e726437
wsywsy <2238574812@qq.com>
2020-07-06 18:50:30 +0800

    citysearch的制作

--- src/pages/city/city.vue
+++ src/pages/city/city.vue
@@ -2,15 +2,19 @@
 <div>
 <cityheader>
 </cityheader>
+<citysearch>
+</citysearch>
 </div>
 </template>
 
 <script>
 import cityheader from './components/header'
+import citysearch from './components/search.vue'
 export default {
   name: 'city',
   components: {
-    cityheader
+    cityheader,
+    citysearch
   }
 }
 </script>
---
+++ src/pages/city/components/search.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="search">
+<input type="text" placeholder="输入城市名或者拼音"/>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'citysearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/variables.styl'
+@import '~styles/mixins.styl'
+.search
+  background: $bgcolor
+  height: .86rem
+  display:flex
+  min-width: 0
+  input
+    box-sizing: border-box
+    height: .58rem
+    display: block
+    width: 85%
+    border: solid 1px white
+    border-radius: 5px
+    text-align: center
+    margin: 0 auto
+    color: #666
+    padding: 0 .1rem
+    align-self:center/*通过设置父元素为flex子元素align-self:center来设置垂直居中*/
+</style>
