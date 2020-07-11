4097f216d6f30d904db0e5c4063feb83f6cdbbe9
wsywsy <2238574812@qq.com>
2020-07-11 16:29:07 +0800

    commongalery公共画廊组件完成

---
+++ src/common/gallery/gallery.vue
@@ -0,0 +1,61 @@
+<template>
+<div class="gallery">
+<div class="wrapper" @click="handleclick">
+<swiper :options="swiperoptions">
+    <swiper-slide v-for="(item, index) in imgs" :key="index">
+    <img class="swiper-image" :src="item"/>
+    </swiper-slide>
+    <div class="swiper-pagination" slot="pagination"></div>
+</swiper>
+</div>
+</div>
+</template>
+<script>
+export default{
+  name: 'commongallery',
+  props: {
+    imgs: {
+      type: Array
+    }
+  },
+  data () {
+    return {
+      swiperoptions: {
+        pagination: '.swiper-pagination',
+        paginationType: 'fraction',
+        loop: true
+      }
+    }
+  },
+  methods: {
+    handleclick () {
+      this.$emit('change')
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+.wrapper>>>.swiper-container
+  overflow: inherit
+.gallery
+  display: flex
+  flex-direction: column
+  justify-content: center
+  z-index: 99
+  position: fixed
+  top: 0
+  right: 0
+  left: 0
+  bottom: 0
+  background: #000
+  .wrapper
+    width: 100%;
+    height: 0
+    padding-bottom: 100%
+    .swiper-pagination
+      color: white
+      font-size: 25px
+      bottom: -1rem
+    .swiper-image
+      width: 100%;
+</style>
--- src/pages/detail/components/banner.vue
+++ src/pages/detail/components/banner.vue
@@ -1,15 +1,38 @@
 <template>
-<div class='banner'>
+<div>
+<div class='banner' @click="handleclick">
 <img class="bannerimage" src="https://imgs.qunarzz.com/sight/p0/1701/88/885ec9c1584a572aa3.img.png_710x360_bf4dab2a.png"/>
 <div class="banner-info">
   <div class="banner-titile">开封府（AAAA景区）</div>
-  <div class="banner-number"><span class="iconfont">&#xe635;</span>39</div>
+  <div class="banner-number"><span class="iconfont">&#xe635;</span>{{this.imgs.length}}</div>
 </div>
+</div>
+<commongallery v-if="show" @change="handlechange" :imgs="imgs"></commongallery>
 </div>
 </template>
 <script>
+import commongallery from 'common/gallery/gallery'
 export default {
-  name: 'detailbanner'
+  name: 'detailbanner',
+  components: {
+    commongallery
+  },
+  data () {
+    return {
+      show: false,
+      imgs: ['https://imgs.qunarzz.com/sight/p0/1701/88/885ec9c1584a572aa3.img.png_710x360_bf4dab2a.png',
+        'https://imgs.qunarzz.com/sight/p0/201403/07/a5232bae168e6120050b0c72b49ed1a9.jpg_710x360_121ff592.jpg', 'https://imgs.qunarzz.com/sight/p0/201403/07/0971f37f898ca09615b4375136c5c381.jpg_710x360_58763fc7.jpg', 'https://imgs.qunarzz.com/sight/p0/201309/16/dd7de3d58f8ac96dc8d65eac.jpg_710x360_8c605ecf.jpg'
+      ]
+    }
+  },
+  methods: {
+    handleclick () {
+      this.show = true
+    },
+    handlechange () {
+      this.show = false
+    }
+  }
 }
 </script>
 <style lang="stylus" scoped>
