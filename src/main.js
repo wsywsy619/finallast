4f7f56f81c0aaf7ac14bda795ddb45ea262e4eda
wsywsy <2238574812@qq.com>
2020-07-14 18:03:31 +0800

    新的脚手架新的开始

--- src/main.js
+++ src/main.js
@@ -3,21 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import fastClick from 'fastclick'
-import '../node_modules/vue-awesome-swiper/node_modules/swiper/dist/css/swiper.css'
+import 'swiper/dist/css/swiper.css'
 import store from './store'// 从store文件夹下引入index.js
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 Vue.config.productionTip = false
-fastClick.attach(document.body)
 Vue.use(VueAwesomeSwiper)
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
   store,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App)
+}).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/pages/Home/Home.vue'
+import Home from '@/pages/home/Home.vue'
 import detail from '@/pages/detail/detail.vue'
 import city from '@/pages/city/city.vue'
 Vue.use(Router)
@@ -10,15 +10,18 @@ export default new Router({
     path: '/',
     name: 'Home',
     component: Home
-  }, {
+  }
+  , {
     path: '/city',
     name: 'city',
     component: city
-  }, {
+  }
+  , {
     path: '/detail/:id',
     name: 'detail',
     component: detail
-  }],
+  }
+  ],
   scrollBehavior (to, from, savedPositon) { // 用于解决路由切换时候停留问题（每次切换到不同的detail页面时候滚动条保持着上一层的页面）
     return {
       x: 0,
